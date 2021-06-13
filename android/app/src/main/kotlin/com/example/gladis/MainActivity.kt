026b2b186b43b60318b02f22ef0eaff454947e7e
Gladis1999 <gladisms1999@gmail.com>
2021-06-13 12:39:12 +0530

    adding files

---
+++ android/app/src/main/kotlin/com/example/gladis/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.gladis
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/HomePage.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:tensorflow/tensorflow.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Home Page'),
+          backgroundColor: Colors.green,
+        ),
+        body: Container(
+          decoration: new BoxDecoration(
+            image: new DecorationImage(
+              image: new AssetImage("assets/images/farmer.jpg"),
+              fit: BoxFit.fill,
+            ),
+          ),
+          child: Center(
+            child: Container(
+              height: 80,
+              width: 150,
+              decoration: BoxDecoration(
+                  color: Colors.green, borderRadius: BorderRadius.circular(10)),
+              child: FlatButton(
+                onPressed: () {
+                  Navigator.push(
+                      context, MaterialPageRoute(builder: (_) => Tensorflow()));
+                },
+                child: Text(
+                  'Welcome',
+                  style: TextStyle(color: Colors.white, fontSize: 25),
+                ),
+              ),
+            ),
+          ),
+        ));
+  }
+}
